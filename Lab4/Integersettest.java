c10d2e0e6bcee583d2c60df9bf932a2ebf60e8b4
Siraphob-06 <siraphob.cha@ku.th>
2025-07-18 18:56:21 +0700

    Add files via upload

--- Lab4/Integersettest.java
+++ Lab4/Integersettest.java
@@ -1,6 +1,8 @@
 public class Integersettest {
     public static void main(String[] args) {
         Integerset a=new Integerset();
+
+        //Case1 การสร้าง การเพิ่ม
         a.add(50);
         a.add(70);
         a.add(40);
@@ -11,6 +13,7 @@ public class Integersettest {
         else{
         System.out.println("Fail"+a.toString());
     }
+    //Case2 การremove
     a.remove(50);
     s = "[40, 60, 70]";
     if(a.toString().equals(s)){
@@ -18,5 +21,7 @@ public class Integersettest {
     }else{
          System.out.println("Failed");
     }
+
+    //Case3 การ
 }
 }
